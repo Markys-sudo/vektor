68abdda19528e13748f460592a38aa539b144238
Markys-sudo <mr.volodkovi4@gmail.com>
2026-06-15 21:33:56 +0300

    create filters, urls, view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
+from django_filters.views import FilterView
 
-# Create your views here.
+from .filters import ProductFilter
+from .models import Product, Category
+
+class ProductListView(FilterView):
+    model = Product
+    filterset_class = ProductFilter
+    template_name = "products/product_list.html"
+    context_object_name = "products"
+    paginate_by = 12
+    
+    def get_queryset(self):
+        return (
+            Product.objects.active()
+            .with_rating()
+            .with_popularity()
+            .select_related("category")
+            .order_by("-created_at")
+        )
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = Category.objects.all()
+        return context
+    
+class ProductDetailView(DetailView):
+    template_name = "products/product_detail.html"
+    context_object_name = "product"
+    
+    def get_queryset(self):
+        return (
+            Product.objects.active()
+            .with_rating()
+            .select_related("category")
+            .prefetch_related("reviews__user")
+        )               
+        
+    def get_context_data(self, **kwargs):
+        
+        context = super().get_context_data(**kwargs)
+        
+        pass #MARK: Add related products, reviews, etc. to context if needed
+    
+        return context
